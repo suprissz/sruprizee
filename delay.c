9ff3d5363838e2acf10dc433e2df21b35b79c5c2
suprissz <117463664+suprissz@users.noreply.github.com>
2022-11-04 21:51:38 +0700

    Create delay.c

---
+++ delay.c
@@ -0,0 +1,10 @@
+#include <chrono>
+#include <thread>
+
+int main() {
+    using namespace std::this_thread; // sleep_for, sleep_until
+    using namespace std::chrono; // nanoseconds, system_clock, seconds
+
+    sleep_for(nanoseconds(10));
+    sleep_until(system_clock::now() + seconds(1));
+}
